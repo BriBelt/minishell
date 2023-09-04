f13c9df239298c3cb6c6c3f083b6535b2537c954
jaimmart <jaimmart@c1r2s3.42madrid.com>
2023-09-04 12:01:25 +0200

    Logic of exec3() aplied to exec_all(),functioning. There was an error in the condition for calling middle_child in exec_all.

--- children.c
+++ children.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   children.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaimmart32 <jaimmart32@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jaimmart <jaimmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 17:18:26 by bbeltran          #+#    #+#             */
-/*   Updated: 2023/09/01 14:59:05 by jaimmart32       ###   ########.fr       */
+/*   Updated: 2023/09/04 11:28:39 by jaimmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,13 @@ t_pipex	first_child(t_pipex pipex, t_command *command, t_shell *mini)
 			pipex.cmd_path = find_comm_path(command->args[0]);
 			close(pipex.pipes[0][0]);
 			if (pipex.in_fd != -1)
-				dup2(pipex.in_fd, STDIN);
+				(dup2(pipex.in_fd, STDIN), close(pipex.in_fd));
 			if (pipex.out_fd != -1)
-				dup2(pipex.out_fd, STDOUT);
+				(dup2(pipex.out_fd, STDOUT), close(pipex.in_fd));
 			else
-			{
-				dup2(pipex.pipes[0][1], STDOUT);
-				close(pipex.pipes[0][1]);
-			}
+				(dup2(pipex.pipes[0][1], STDOUT), close(pipex.pipes[0][1]));
 			if (!call_builtins(command, mini)
-					&& execve(pipex.cmd_path, command->args, mini->envp) == -1)
+				&& execve(pipex.cmd_path, command->args, mini->envp) == -1)
 				printf("Execve: %s: command not found\n", command->args[0]);
 		}
 	}
@@ -61,24 +58,24 @@ t_pipex	first_child(t_pipex pipex, t_command *command, t_shell *mini)
 t_pipex	middle_child(t_pipex pipex, t_command *command, t_shell *mini, int i)
 {
 	if (is_command(command->args[0]) == COMMAND
-			|| is_builtin(command->args[0]) == BUILTIN)
+		|| is_builtin(command->args[0]) == BUILTIN)
 	{
 		pipex.child_id[i] = fork();
 		if (!pipex.child_id[i])
 		{
 			get_file_des(&pipex, command->redirect);
 			pipex.cmd_path = find_comm_path(command->args[0]);
-			(close(pipex.pipes[i - 1][1]), close(pipex.pipes[i][0]));
+			close(pipex.pipes[i][0]);
 			if (pipex.in_fd != -1)
-				dup2(pipex.in_fd, STDIN);
+				dup2(pipex.in_fd, STDIN), close(pipex.in_fd);
 			else
-				dup2(pipex.pipes[i - 1][0], STDIN);
+				(dup2(pipex.pipes[i - 1][0], STDIN), close(pipex.pipes[i - 1][0]));
 			if (pipex.out_fd != -1)
 				dup2(pipex.out_fd, STDOUT);
 			else
-				dup2(pipex.pipes[i][1], STDOUT);
+				dup2(pipex.pipes[i][1], STDOUT), close(pipex.pipes[i][1]);
 			if (!call_builtins(command, mini)
-					&& execve(pipex.cmd_path, command->args, mini->envp) == -1)
+				&& execve(pipex.cmd_path, command->args, mini->envp) == -1)
 				printf("Execve: %s: command not found\n", command->args[0]);
 		}
 	}
@@ -97,11 +94,10 @@ t_pipex	last_child(t_pipex pipex, t_command *command, t_shell *mini, int i)
 		{
 			get_file_des(&pipex, command->redirect);
 			pipex.cmd_path = find_comm_path(command->args[0]);
-			close(pipex.pipes[i - 1][1]);
 			if (pipex.in_fd != -1)
 				dup2(pipex.in_fd, STDIN);
 			else
-				dup2(pipex.pipes[i - 1][0], STDIN);
+				(dup2(pipex.pipes[i - 1][0], STDIN), close(pipex.pipes[i - 1][0]));
 			if (pipex.out_fd != -1)
 				dup2(pipex.out_fd, STDOUT);
 			if (!call_builtins(command, mini)
--- execution.c
+++ execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaimmart32 <jaimmart32@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jaimmart <jaimmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:26:35 by bbeltran          #+#    #+#             */
-/*   Updated: 2023/09/01 16:36:34 by jaimmart32       ###   ########.fr       */
+/*   Updated: 2023/09/04 11:57:03 by jaimmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,18 +122,19 @@ t_pipex execute_all(t_shell *mini, t_pipex pipex, int count)
 				pipe(pipex.pipes[i]);
 			if (i == 0)
 			{
-
 				pipex = first_child(pipex, cmd, mini);
+				close(pipex.pipes[i][1]);
 			}
 			else if (i == count - 1)
 			{
-//				close(pipex.pipes[i - 1][1]);
 				pipex = last_child(pipex, cmd, mini, i);
+				close(pipex.pipes[i - 1][0]);
 			}
-			else if (i < 0 && i < count - 1)
+			else if (i > 0 && i < count - 1)
 			{
-				
 				pipex = middle_child(pipex, cmd, mini, i);
+				close(pipex.pipes[i - 1][0]);
+				close(pipex.pipes[i][1]);
 			}
 			i++;
 			cmd = cmd->next;
@@ -144,8 +145,8 @@ t_pipex execute_all(t_shell *mini, t_pipex pipex, int count)
 
 void executor(t_shell *mini)
 {
-	t_pipex pipex;
-	int count;
+	t_pipex	pipex;
+	int		count;
 
 	pipex = pipex_init();
 	count = command_counter(mini->cmds);
@@ -153,13 +154,14 @@ void executor(t_shell *mini)
 		pipex = execute_one(mini, pipex);
 	else if (count == 2)
 		pipex = execute_two(mini, pipex);
-	else if (count == 3)
-		execute_three(mini, pipex);
+//	else if (count == 3)
+//		execute_three(mini, pipex);
 	else
 		pipex = execute_all(mini, pipex, count);
 	//	wait_for_child(pipex, count);
 	waitpid(pipex.child_id[0], NULL, 0);
 	waitpid(pipex.child_id[1], NULL, 0);
 	waitpid(pipex.child_id[2], NULL, 0);
+	waitpid(pipex.child_id[3], NULL, 0);
 	//	waitpid(-1, NULL, 0);
 }
